8eff4d6419fbb36055871e5e2d8303ce943d6bd9
Tarique Ejaz <tariqueejaz.93102@gmail.com>
2019-11-06 23:36:00 +0700

    update async/await code

--- web-scraping-101/index.js
+++ web-scraping-101/index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+// const request = require('request-promise');
 const cheerio = require('cheerio');
 
 const scrapeWebsiteData = (url) => {
@@ -24,9 +25,49 @@ const scrapeWebsiteData = (url) => {
 
         Product.FeatureDetails = featureDetails;
 
+        console.log(Product);
+
         return Product;
 
     });
 };
 
 scrapeWebsiteData('https://www.amazon.in/Apple-MacBook-Pro-8th-Generation-Intel-Core-i5/dp/B07S8D1K3M/ref=sr_1_1_sspa?crid=PXJV2CGQP1HH&keywords=macbook+pro+13+inch&qid=1572790136&sprefix=macbook%2Caps%2C406&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUE0SUtNODI5UVJFR04mZW5jcnlwdGVkSWQ9QTAxMzM5MTczTEVCUkNYQ1JPNUgxJmVuY3J5cHRlZEFkSWQ9QTAxNjAzMTAyWkxNUkxBUU5ER01FJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==');
+
+// using request-promise and async/await
+
+// const scrapeWebsiteData = async (url) => {
+//     var options = {
+//         uri: url,
+//         transform: function (body) {
+//             return cheerio.load(body);
+//         }
+//     };
+
+//     const $ = await request(options);
+//     const Product = {};
+
+//     Product.Title = $('#productTitle').text().replace(/\s\s+/g, '');
+//     Product.PriceText = $('#priceblock_ourprice').text();
+//     Product.Image = $('#landingImage').attr('data-old-hires');
+
+//     // calculate the price in terms of value
+//     let price = parseInt(Product.PriceText.slice(2, Product.PriceText.length).replace(/,/g, ''));
+//     Product.PriceValue = price;
+
+//     // fetch feature details
+//     let featureDetails = [];
+
+//     $('#feature-bullets>ul>li>span').each((i, el) => {
+//         featureDetails.push($(el).text().replace(/[\n\t]/g, '').trim());
+//     });
+
+//     Product.FeatureDetails = featureDetails;
+
+//     return Product;
+// };
+
+// scrapeWebsiteData('https://www.amazon.in/Apple-MacBook-Pro-8th-Generation-Intel-Core-i5/dp/B07S8D1K3M/ref=sr_1_1_sspa?crid=PXJV2CGQP1HH&keywords=macbook+pro+13+inch&qid=1572790136&sprefix=macbook%2Caps%2C406&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUE0SUtNODI5UVJFR04mZW5jcnlwdGVkSWQ9QTAxMzM5MTczTEVCUkNYQ1JPNUgxJmVuY3J5cHRlZEFkSWQ9QTAxNjAzMTAyWkxNUkxBUU5ER01FJndpZGdldE5hbWU9c3BfYXRmJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ==')
+//     .then((scrapedData) => {
+//         console.log(scrapedData);
+//     });
